52951a8f6f408c732cde874005ca239f169e0e64
MoizArif78987 <106541549+MoizArif78987@users.noreply.github.com>
2022-05-31 11:15:40 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
 import React from "react";
-const App = () => <h1>First Page Using React</h1>
+
+class App extends React.Component
+{
+  render()
+  {
+    return <h1 className="container">Hello World this is a work on react usin JSX className</h1>
+  }
+} 
+
 export default App
